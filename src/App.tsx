dda08316416050b4b2465894461b2348102a9787
sulash.manandhar <sulash.manandhar@gmail.com>
2025-10-07 19:44:00 +0545

    feat(theme): add text colors and update app demo
    add new color variables for text, muted, warning, default and danger
    update App component to showcase new color classes and improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 export default function App() {
   return (
-    <div>
-      App
-      <p className="text-2xl">Regular</p>
-      <p className="text-2xl font-medium">Medium</p>
-      <p className="text-2xl font-semibold">Semibold</p>
-      <p className="text-2xl font-bold">Bold</p>
+    <div className="flex flex-col gap-8">
+      <div>
+        <h2>Fonts</h2>
+        <p className="text-2xl">Regular</p>
+        <p className="text-2xl font-medium ">Medium</p>
+        <p className="text-2xl font-semibold ">Semibold</p>
+        <p className="text-2xl font-bold ">Bold</p>
+      </div>
+      <div>
+        <h2>Colors</h2>
+        <p className="text-default">Blue</p>
+        <p className="text-danger">Red</p>
+        <p className="text-warning">Yellow</p>
+        <p className="text-muted">Muted</p>
+      </div>
     </div>
   );
 }
